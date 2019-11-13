3935a109c9f05a6a2e421d4a23573f1a338a12df
Ethan Anderson <eanderson@vivial.net>
2019-11-13 13:33:54 -0600

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  title: 'Ethan Anderson',
+  description: 'Personal Website of Ethan Anderson',
+  themeConfig: {
+    lastUpdated: 'Last Updated',
+    logo: '/eanderson.jpg',
+    nav: [
+      { text: 'About', link: '/#about-me' },
+      { text: 'Resume', link: '/#resume' },
+      { text: 'Contact', link: '/#contact' },
+    ],
+    smoothScroll: true
+  }
+}
